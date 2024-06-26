c9977de548a4b67cafd1a8e7ff5615c6421a8dc0
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-06-26 12:40:16 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "Prince2090.be22@chitkara.edu.in"
+var accountPassword = "1234"
+accountCity = "sirsa"
+
+
+accountEmail ="next@gmail.com"
+accountPassword="54321"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
