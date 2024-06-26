a23637ef187da5af9764e8742f776625e568b2dc
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-06-26 17:40:19 +0530

    conversionjs

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score = "55abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "55" = 55 
+// "33abc "= NAN
+// true  = 1
+// false  = 0 
+
+let isLoggedIn = ""
+let boolenislogedin = Boolean(isLoggedIn)
+console.log(boolenislogedin)
+
+
+
+
+let somenumber = 33 
+let stringIsSomeNumber = String(somenumber)
+console.log(typeof somenumber)
+console.log(stringIsSomeNumber);
