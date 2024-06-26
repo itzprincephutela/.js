349caf23f39711b1cee5209301a2e0232408a2a3
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-06-26 17:12:00 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+console.log(3 + 3)
+console.log("Prince")
+let name = "Prince "
+let age = 15
+let isLoggedIn = true 
+console.log(typeof age)
+console.log(typeof null)
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("prince")
+const accountId = 144553
+let accountEmail = "Prince2090.be22@chitkara.edu.in"
+var accountPassword = "1234"
+accountCity = "sirsa"
+accountEmail ="next@gmail.com"
+accountPassword="54321"
+console.table([accountId,accountEmail,accountPassword,accountCity ]);
