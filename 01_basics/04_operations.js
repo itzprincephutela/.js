9cbf59f00ab59f588bd19e6e572cf5816d1a6624
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-06-27 21:05:38 +0530

    27JUNE

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,17 @@
+let value = 3
+let negvalue =-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+
+// let str1 = "hello"
+// let str2 = " Prince"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+let gamecounter = 10 ;
+++gamecounter;
+console.log(gamecounter);
