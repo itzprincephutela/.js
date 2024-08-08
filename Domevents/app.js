2188ff5677db6653d254db14cabc6477f7ac97fe
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-08-08 23:28:14 +0530

    commit

--- Domevents/app.js
+++ Domevents/app.js
@@ -1,40 +1,154 @@
-// let btns = document.querySelectorAll("button");
-
-// for (let btn of btns) {
-//     // btn.onclick = sayHello;
-//     // btn.onmouseenter = function (){
-//     //     console.dir("You Entered");
-//     // }
-//     // console.dir(btn);
+let from = document.querySelector("form");
+form.addEventListener("submit",function(){
+    console.log("Form Submit : "); 
+})
 
 
-//     // btn.addEventListener("click",sayHello);
-//     // btn.addEventListener("click",sayName); 
-//     btn.addEventListener("dblclick",function(){
-//         console.log("HI");
-//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inp = document.querySelector("input");
+
+// // inp.addEventListener("keydown",function(event){
+// //     console.log(event);
+// //     console.log("Key was press");
+// // })
+// // inp.addEventListener("keyup",function(){
+// //     console.log("Key was released");
+// // })
+
+// inp.addEventListener("keydown",function(){
+//     console.log("code = ",event.code);
+//     if(event.code =="ArrowUp"){
+//         console.log("Char move Forward");
+//     }
+//     else if(event.code =="ArrowDown"){
+//         console.log("Char move Forward");
+//     }
+//     else if(event.code =="ArrowLeft"){
+//         console.log("Char move Forward");
+//     }
+//    else if(event.code =="ArrowRight"){
+//         console.log("Char move Forward");
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let btns = document.querySelectorAll("button");
+
+// // for (let btn of btns) {
+// //     // btn.onclick = sayHello;
+// //     // btn.onmouseenter = function (){
+// //     //     console.dir("You Entered");
+// //     // }
+// //     // console.dir(btn);
+
+
+// //     // btn.addEventListener("click",sayHello);
+// //     // btn.addEventListener("click",sayName); 
+// //     btn.addEventListener("dblclick",function(){
+// //         console.log("HI");
+// //     })
+// // }
+
+// // function sayHello() {
+// //     alert("Hello World");
+// // }
+// // function sayName(){
+// //     alert("PRince Kumarr");
+// // }
+
+
+// let btn = document.querySelector("button");
+// let p = document.querySelector("p");
+// let hea = document.querySelector("h1");
+
+
+
+// btn.addEventListener("click", colorchange);
+// p.addEventListener("click", colorchange);
+// hea.addEventListener("click", colorchange);
+
+// function colorchange() {
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue";
 // }
-
-// function sayHello() {
-//     alert("Hello World");
-// }
-// function sayName(){
-//     alert("PRince Kumarr");
-// }
-
-
-let btn = document.querySelector("button");
-let p = document.querySelector("p");
-let hea = document.querySelector("h1");
-
-
-
-btn.addEventListener("click", colorchange);
-p.addEventListener("click", colorchange);
-hea.addEventListener("click", colorchange);
-
-function colorchange() {
-    console.dir(this.innerText);
-    this.style.backgroundColor = "blue";
-}
 
