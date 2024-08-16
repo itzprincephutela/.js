6d17943f6bdcd519eb452df18d2c3f5a140c93b4
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-08-16 22:08:38 +0530

    Update 07_string.js

--- 01_basics/07_string.js
+++ 01_basics/07_string.js
@@ -4,7 +4,10 @@
 
 // // console.log(name + repoCount + " Value");
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 // const gameName = new String('hitesh-hc-com')
 
