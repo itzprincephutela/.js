839d01ec8a411d14f6c2705f145549a5a7e6c68b
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-09-24 23:15:14 +0530

    Update iteraton.js

--- 03_Loops/iteraton.js
+++ 03_Loops/iteraton.js
@@ -4,7 +4,7 @@ coding.forEach( (item) => {
 console.log(item); 
 })
 
-//  if u want to make in array 
+//  hry if u want to make in array 
 const mynum= [1,2,3,4,5,6,7,8,9,10];
 let newnums=mynum.filter((num)=> num > 5);
 console.log(newnums);
